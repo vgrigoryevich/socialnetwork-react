1113ac82e16e6fdaaef12a1350d2b23b896ca841
vgrigoryevich <v.grigoryevich@gmail.com>
2020-03-24 16:55:53 +0200

    create repo + added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import style from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to the party</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
